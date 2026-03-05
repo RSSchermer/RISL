da1cf72c74a287134e889bd3c5c5389a9dba0452
rsschermer <roland0507@gmail.com>
2026-03-05 15:17:23 +0100

    Refactor the run-behavioral-tests script into an xtask

---
+++ xtask/src/main.rs
@@ -0,0 +1,41 @@
+use anyhow::Result;
+use clap::Parser;
+use xshell::{cmd, Shell};
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(clap::Subcommand)]
+enum Commands {
+    /// Runs behavioral tests
+    RunBehavioralTests,
+}
+
+fn main() -> Result<()> {
+    let cli = Cli::parse();
+    let sh = Shell::new()?;
+
+    match &cli.command {
+        Commands::RunBehavioralTests => run_behavioral_tests(&sh),
+    }
+}
+
+fn run_behavioral_tests(sh: &Shell) -> Result<()> {
+    println!("Recompiling rislc to ensure it is up to date with the current source...");
+    cmd!(sh, "cargo build --bin=rislc")
+        .env("RUSTFLAGS", "-Awarnings")
+        .run()?;
+
+    println!("Running behavioral tests...");
+    cmd!(sh, "cargo test --package=behavioral-tests")
+        .env("RUSTFLAGS", "-Awarnings")
+        .env("RUST_BACKTRACE", "full")
+        .env("RUSTC_WRAPPER", "target/debug/rislc")
+        .run()?;
+
+    Ok(())
+}
