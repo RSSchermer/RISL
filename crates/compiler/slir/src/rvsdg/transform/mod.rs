b80c2083b8c475cfa3220a09c012066a3b4bcee4
rsschermer <roland0507@gmail.com>
2026-03-25 17:57:43 +0100

    Add invalid-ptr-replacement transform

--- crates/compiler/slir/src/rvsdg/transform/mod.rs
+++ crates/compiler/slir/src/rvsdg/transform/mod.rs
@@ -8,6 +8,7 @@ pub mod duplicate_switch_input_elimination;
 pub mod duplicate_switch_output_elimination;
 pub mod enum_replacement;
 pub mod function_inlining;
+mod invalid_ptr_replacement;
 pub mod memory_promotion_and_legalization;
 pub mod memory_transform;
 pub mod offset_slice_elaboration;
@@ -38,6 +39,7 @@ pub fn transform(module: &mut Module, rvsdg: &mut Rvsdg) {
     pred_to_case_to_pred_merging::transform_entry_points(module, rvsdg);
     switchify_pred_to_case::transform_entry_points(module, rvsdg);
     common_value_elimination::transform_entry_points(module, rvsdg);
+    invalid_ptr_replacement::transform_entry_points(module, rvsdg);
     switch_arg_reduction::transform_entry_points(module, rvsdg);
     const_switch_inlining::transform_entry_points(module, rvsdg);
     switch_merging::transform_entry_points(module, rvsdg);
