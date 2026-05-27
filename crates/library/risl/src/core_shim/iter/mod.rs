5cc44a85f9fcfa3edac53f0b94a58f356965ab3e
rsschermer <roland0507@gmail.com>
2026-05-27 12:34:01 +0200

    Remove more unnecessary Iterator method shims

--- crates/library/risl/src/core_shim/iter/mod.rs
+++ crates/library/risl/src/core_shim/iter/mod.rs
@@ -1,40 +1,6 @@
 use std::num::NonZero;
-use std::ops::Try;
 
 use risl_macros::gpu;
-
-#[gpu]
-#[cfg_attr(rislc, rislc::core_shim("core::iter::Iterator::fold"))]
-pub fn iterator_fold<I, B, F>(mut iter: I, init: B, mut f: F) -> B
-where
-    I: Iterator,
-    F: FnMut(B, I::Item) -> B,
-{
-    let mut accum = init;
-
-    while let Some(x) = Iterator::next(&mut iter) {
-        accum = f(accum, x);
-    }
-
-    accum
-}
-
-#[gpu]
-#[cfg_attr(rislc, rislc::core_shim("core::iter::Iterator::try_fold"))]
-pub fn iterator_try_fold<I, B, F, R>(iter: &mut I, init: B, mut f: F) -> R
-where
-    I: Iterator,
-    F: FnMut(B, I::Item) -> R,
-    R: Try<Output = B>,
-{
-    let mut accum = init;
-
-    while let Some(x) = Iterator::next(iter) {
-        accum = f(accum, x)?;
-    }
-
-    R::from_output(accum)
-}
 
 #[gpu]
 #[cfg_attr(rislc, rislc::core_shim("core::iter::Iterator::count"))]
@@ -43,29 +9,6 @@ where
     I: Iterator,
 {
     iter.fold(0, |count, _| count + 1)
-}
-
-#[gpu]
-#[cfg_attr(rislc, rislc::core_shim("core::iter::Iterator::last"))]
-pub fn iterator_last<I>(iter: I) -> Option<I::Item>
-where
-    I: Iterator,
-{
-    fn some<T>(_: Option<T>, x: T) -> Option<T> {
-        Some(x)
-    }
-
-    iterator_fold(iter, None, some)
-}
-
-#[gpu]
-#[cfg_attr(rislc, rislc::core_shim("core::iter::Iterator::nth"))]
-pub fn iterator_nth<I>(iter: &mut I, n: usize) -> Option<I::Item>
-where
-    I: Iterator + ?Sized,
-{
-    iterator_advance_by(iter, n).ok()?;
-    iter.next()
 }
 
 #[gpu]
