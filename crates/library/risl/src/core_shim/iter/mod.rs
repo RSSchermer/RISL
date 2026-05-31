b16a2bcaca909c22aaa3d266b0bf9f24dc732c2c
rsschermer <roland0507@gmail.com>
2026-05-31 22:50:48 +0200

    Remove arith ops shims and Iterator::count shim, as these are no longer necessary

--- crates/library/risl/src/core_shim/iter/mod.rs
+++ crates/library/risl/src/core_shim/iter/mod.rs
@@ -2,21 +2,6 @@ use std::num::NonZero;
 
 use risl_macros::gpu;
 
-// The body of this shim is mostly identical to the body of the regular Rust `core` version, except
-// in that the regular Rust `core` version decorates the fold closure with
-// `#[rustc_inherit_overflow_checks]`. This causes rustc to insert an `Assert` terminator, which we
-// don't support in RISL. The RISL pass already disables overflow checks through the session config,
-// however, the presence of a `#[rustc_inherit_overflow_checks]` attribute overrides this and forces
-// an overflow check regardless. Hence the need for this shim.
-#[gpu]
-#[cfg_attr(rislc, rislc::core_shim("core::iter::Iterator::count"))]
-pub fn iterator_count<I>(iter: I) -> usize
-where
-    I: Iterator,
-{
-    iter.fold(0, |count, _| count + 1)
-}
-
 // The body of this shim is identical to the body of the regular Rust `core` version. We still have
 // to shim this function, because of MIR optimizations performed by rustc when compiling core/std.
 // In this particular case, it will inline NonZero::new, which exposes a transmute that rislc cannot
